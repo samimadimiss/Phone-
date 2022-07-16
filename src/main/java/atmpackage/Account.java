e885ea2068a8256189b62788d8bfbb11b8118283
Nancy <samimadimiss@gmail.com>
2022-07-16 14:04:40 -0400

    first commit

---
+++ src/main/java/atmpackage/Account.java
@@ -0,0 +1,12 @@
+package atmpackage;
+
+public class Account {
+    private int accountNumber;
+    private int pinNumber;
+    private double checkingBalance;
+    private double savingBalance;
+
+
+
+
+}
